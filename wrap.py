31185ed39c12f695813b2a972320567ea3f3caaf
Pierre de Tournemire <pierredet@gmail.com>
2017-01-16 10:07:17 +0100

    remove old code from wrap.py

--- wrap.py
+++ wrap.py
@@ -12,15 +12,13 @@ basepath = os.path.dirname(os.path.abspath(__file__))
 dllspath = os.path.join(basepath, 'libdarknet.so')
 os.environ['PATH'] = dllspath + os.pathsep + os.environ['PATH']
 
-libdarknet = ctypes.cdll.LoadLibrary(os.path.join(basepath, 'libdarknet.so'))
+libdarknet = ctypes.cdll.LoadLibrary(dllspath)
 
-
-c_float_p = ctypes.POINTER(ctypes.c_float)
 
 array_1d_float = npct.ndpointer(dtype=np.float32, ndim=1, flags='CONTIGUOUS')
 
 # set the argument type
-# (char *datacfg, char *cfgfile, char *weightfile, float* data, int h, int w, int c, float thresh, float hier_thresh)
+# (char *datacfg, char *cfgfile, char *weightfile, float* data, int h, int w, int c, float thresh, float hier_thresh, float* out)
 libdarknet.test_detector_python.argtypes = [ctypes.c_char_p, ctypes.c_char_p,
                                             ctypes.c_char_p, array_1d_float,
                                             ctypes.c_int, ctypes.c_int,
@@ -28,7 +26,7 @@ libdarknet.test_detector_python.argtypes = [ctypes.c_char_p, ctypes.c_char_p,
                                             ctypes.c_float, ctypes.c_float, array_1d_float]
 
 # set the return type
-#libdarknet.test_detector_python.restype = c_float_p
+
 BATCH_SIZE = 64
 
 def detector(datacfg, cfgfile, weightfile, data, h, w, c, thresh, hier_thresh):
@@ -54,12 +52,10 @@ def detector(datacfg, cfgfile, weightfile, data, h, w, c, thresh, hier_thresh):
     return out
 
 imgdir = "/home/pierre/lego-assembly-helper/dataset/output_dataset/images"
-batch = np.empty((BATCH_SIZE, 416, 416, 3), dtype='float32')
-for i, image in enumerate(os.listdir(imgdir)[:BATCH_SIZE]):
-    im = PIL.Image.open(os.path.join(imgdir, image))
-    batch[i] = np.array(im.resize((416, 416))).astype(np.float32)
+image = os.listdir(imgdir)[0]
+im = np.array(PIL.Image.open(os.path.join(imgdir, image))).astype('float32')
 
 res = detector("mul.data", "yolo-mul2.cfg",
                "/home/pierre/backup/yolo-mul_16000.weights",
-               batch, im.size[0], im.size[1], 3, 0.24, 0.5)
-import pdb; pdb.set_trace()
+               im, im.size[0], im.size[1], 3, 0.24, 0.5)
+
